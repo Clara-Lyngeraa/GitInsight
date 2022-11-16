c0cae33d486c1894a72cab63c6c3260ab9fe4dcd
monicahardt <monha@itu.dk>
2022-11-16 16:31:56 +0100

    fucked up

--- GitInsight.Client/Program.cs
+++ GitInsight.Client/Program.cs
@@ -7,12 +7,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddHttpClient("GitInsight.ServerAPI", client => client.BaseAddress = new Uri("https://localhost:7044/api/"));
+builder.Services.AddHttpClient("GitInsight.ServerAPI", client => client.BaseAddress = new Uri("https://localhost:7241"));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("GitInsight.ServerAPI"));
+//builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("GitInsight.ServerAPI"));
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7044/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7241") });
+
 
 
 
--- GitInsight.Server/Program.cs
+++ GitInsight.Server/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+//builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -41,4 +41,4 @@ app.MapFallbackToFile("index.html");
 app.Run();
 
 
-public partial class Program { }
+
