7ba889bb0c66a98b9e36a975fd0310282b13bb0c
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-11-16 16:08:16 +0100

    intet virker

--- GitInsight.Server/Program.cs
+++ GitInsight.Server/Program.cs
@@ -1,33 +1,40 @@
 using GitInsight.Entities;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using GitInsight.Server.Controllers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<GitInsightContext>();
 builder.Services.AddScoped<IAnalyzedRepoRepository, AnalyzedRepoRepository>();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+    
+
+    
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
