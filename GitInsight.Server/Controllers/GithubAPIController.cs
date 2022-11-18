6eaac128c42cbaf8f8a277d477b97b78d8741114
monicahardt <monha@itu.dk>
2022-11-18 16:14:22 +0100

    Everything runs here. Go back if you screw up

--- GitInsight.Server/Controllers/GithubAPIController.cs
+++ GitInsight.Server/Controllers/GithubAPIController.cs
@@ -1,46 +1,46 @@
-using Microsoft.AspNetCore.Mvc;
-using Octokit;
+// using Microsoft.AspNetCore.Mvc;
+// using Octokit;
 
-namespace GitInsight.Server.Controllers;
+// namespace GitInsight.Server.Controllers;
 
 
-[ApiController]
-[Route("[controller]")]
+// [ApiController]
+// [Route("[controller]")]
 
-public class GithubAPIController: ControllerBase{
+// public class GithubAPIController: ControllerBase{
 
     
 
-    public GithubAPIController(){
+//     public GithubAPIController(){
 
-        var client = new GitHubClient(new ProductHeaderValue("hej"));
-        var tokenAuth = new Credentials("PAT_Token");
-        client.Credentials = tokenAuth;
-        var data = client.Repository.GetAllForCurrent();
+//         var client = new GitHubClient(new ProductHeaderValue("hej"));
+//         var tokenAuth = new Credentials("PAT_Token");
+//         client.Credentials = tokenAuth;
+//         var data = client.Repository.GetAllForCurrent();
         
 
 
 
-        // client = new HttpClient();
-        // client.BaseAddress = new Uri("https://api.github.com");
-        // var token = "<token>";
+//         // client = new HttpClient();
+//         // client.BaseAddress = new Uri("https://api.github.com");
+//         // var token = "<token>";
 
-        // client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
-        // client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-        // client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+//         // client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+//         // client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+//         // client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
 
         
 
-        //https://gist.github.com/MaximRouiller/74ae40aa994579393f52747e78f26441
+//         //https://gist.github.com/MaximRouiller/74ae40aa994579393f52747e78f26441
 
-    }
+//     }
 
 
-    //https://docs.github.com/en/rest/repos/forks#list-forks
-    [HttpGet]
-    public async Task<IEnumerable<string>> getForks(string owner, string repo){
+//     //https://docs.github.com/en/rest/repos/forks#list-forks
+//     [HttpGet]
+//     public async Task<IEnumerable<string>> getForks(string owner, string repo){
 
-        //var response = await client.GetAsync($"/repos/{owner}/{repo}/forks");
-        return new List<string>{"hej"};
-    }
-}
+//         //var response = await client.GetAsync($"/repos/{owner}/{repo}/forks");
+//         return new List<string>{"hej"};
+//     }
+// }
